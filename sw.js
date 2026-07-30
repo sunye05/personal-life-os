1bd1d738d636926631df77978be783d4e04bd912
Zixuan Deploy <deploy@zixuan.local>
2026-07-30 20:55:02 +0800

    ci: publish installable app with GitHub Pages

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-﻿const CACHE='life-os-v7';const ASSETS=['/','/index.html','/css/main.css','/js/app.js','/img/favicon.svg','/manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))))});
-
-
-
-
-
-
+﻿const CACHE='life-os-v8';const BASE=new URL('./',self.location).pathname;const A=['','index.html','css/main.css','js/app.js','img/favicon.svg','manifest.webmanifest'].map(x=>BASE+x);self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match(BASE+'index.html'))))});
